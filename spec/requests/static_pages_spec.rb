641cfcd0f0f9d8af718e3803c4ebb4accfc1c569
gableh <gable.heng@gmail.com>
2014-12-03 18:16:19 +0800

    add a contacts page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+let(:result){page.should have_selector('title',:text)}
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title',:text => "tutorial | Home")
+	page.should have_selector('title',:text => "tutorial | Home")
     end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
 	visit '/static_pages/help'
+
 	page.should have_selector('title',:text => "tutorial | Help")
 
      end
@@ -22,5 +25,10 @@ describe "StaticPages" do
 	page.should have_selector('title',:text => "tutorial | About")
 	end
     end
-
+  describe "Contacts" do
+    it "should have the content 'Contacts'" do
+	visit '/static_pages/contacts'
+	page.should have_selector('title',:text => "tutorial | Contacts")
+	end
+    end
 end
